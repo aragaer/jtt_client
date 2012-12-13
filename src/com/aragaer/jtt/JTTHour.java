4a29719495c156fcbf9f57ec8e9f549f0966ec8a
Ilya Konovalov <aragaer@gmail.com>
2012-12-13 20:37:41 +0400

    Use 3-component hours

--- src/com/aragaer/jtt/JTTHour.java
+++ src/com/aragaer/jtt/JTTHour.java
@@ -4,25 +4,49 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class JTTHour implements Parcelable {
-    public static final String Glyphs[] = { "酉", "戌", "亥", "子", "丑", "寅", "卯",
-            "辰", "巳", "午", "未", "申" };
+	public static final int QUARTERS = 4;
+	public static final int PARTS = 100; // split quarter to that much parts
 
-    public int num; // 0 to 11, where 0 is hour of Cock and 11 is hour of Monkey
-    public int fraction; // 0 to 99
+	/* compatibility calculations */
+	private static final int COMPAT_COEFF = QUARTERS * PARTS / 100;
+
+	public static final String Glyphs[] = { "酉", "戌", "亥", "子", "丑", "寅", "卯",
+		"辰", "巳", "午", "未", "申" };
+
+	public boolean isNight;
+	public int num; // 0 to 11, where 0 is hour of Cock and 11 is hour of Monkey
+	public int percent; // 0 to 99 - compatibility
+	public int quarter; // 0 to 3
+	public int quarter_parts; // 0 to PARTS
 
     public JTTHour(int num) {
-        this(num, 0);
+		this(num, QUARTERS / 2, 0);
     }
 
     public JTTHour(int n, int f) {
         this.setTo(n, f);
     }
 
-    // Instead of reallocation, reuse existing object
-    public void setTo(int n, int f) {
-        num = n;
-        fraction = f;
-    }
+	public JTTHour(int n, int q, int f) {
+		this.setTo(n, q, f);
+	}
+
+	// compatibility method
+	public void setTo(int n, int f) {
+		f *= COMPAT_COEFF;
+		int q = f / PARTS;
+		f -= q * PARTS;
+		setTo(n, q, f);
+	}
+
+	// Instead of reallocation, reuse existing object
+	public void setTo(int n, int q, int f) {
+		num = n;
+		isNight = n < 6;
+		quarter = q;
+		quarter_parts = f;
+		percent = (q * PARTS + quarter_parts) / COMPAT_COEFF;
+	}
 
 	public static final Parcelable.Creator<JTTHour> CREATOR = new Parcelable.Creator<JTTHour>() {
 		public JTTHour createFromParcel(Parcel in) {
@@ -36,8 +60,9 @@ public class JTTHour implements Parcelable {
 
 	private JTTHour(Parcel in) {
 		final int n = in.readInt();
+		final int q = in.readInt();
 		final int f = in.readInt();
-		setTo(n, f);
+		setTo(n, q, f);
 	}
 
 	@Override
@@ -48,6 +73,7 @@ public class JTTHour implements Parcelable {
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
 		dest.writeInt(num);
-		dest.writeInt(fraction);
+		dest.writeInt(quarter);
+		dest.writeInt(quarter_parts);
 	}
 }
