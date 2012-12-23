6fbd29d731b5702df1e6229628ffab7f8148fff9
Ilya Konovalov <aragaer@gmail.com>
2012-12-23 04:39:29 +0400

    Simpler JttReceiver

--- src/com/aragaer/jtt/JttReceiver.java
+++ src/com/aragaer/jtt/JttReceiver.java
@@ -1,30 +1,15 @@
 package com.aragaer.jtt;
 
-import java.lang.ref.WeakReference;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 
 public abstract class JttReceiver extends BroadcastReceiver {
-	private WeakReference<Context> ctx;
 	public final static String TICK_ACTION = "com.aragaer.jtt.ACTION_JTT_TICK";
-	private final static IntentFilter filter = new IntentFilter(TICK_ACTION);
+	public final static IntentFilter filter = new IntentFilter(TICK_ACTION);
 
 	abstract void handle_tick(int n, int q, int f);
-
-	void register(Context context) {
-		if (ctx != null)
-			throw new IllegalStateException("Already registered");
-		ctx = new WeakReference<Context>(context);
-		context.registerReceiver(this, filter);
-	}
-
-	void unregister() {
-		ctx.get().unregisterReceiver(this);
-		ctx = null;
-	}
 
 	public void onReceive(Context context, Intent intent) {
 		JTTHour h = intent.getParcelableExtra("jtt");
