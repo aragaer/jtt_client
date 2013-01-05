c250920555c1267c5395a119ec39ae8d97be787b
Ilya Konovalov <aragaer@gmail.com>
2013-01-05 17:23:57 +0400

    JttReceiver can now re-apply last broadcast

--- src/com/aragaer/jtt/JttReceiver.java
+++ src/com/aragaer/jtt/JttReceiver.java
@@ -8,11 +8,18 @@ import android.content.IntentFilter;
 public abstract class JttReceiver extends BroadcastReceiver {
 	public final static String TICK_ACTION = "com.aragaer.jtt.ACTION_JTT_TICK";
 	public final static IntentFilter filter = new IntentFilter(TICK_ACTION);
+	private JTTHour last;
 
 	abstract void handle_tick(int n, int q, int f);
 
 	public void onReceive(Context context, Intent intent) {
-		JTTHour h = intent.getParcelableExtra("jtt");
-		handle_tick(h.num, h.quarter, h.quarter_parts);
+		last = intent.getParcelableExtra("jtt");
+		handle_last();
+	}
+
+	public void handle_last() {
+		if (last == null)
+			throw new IllegalStateException("No tick yet");
+		handle_tick(last.num, last.quarter, last.quarter_parts);
 	}
 }
