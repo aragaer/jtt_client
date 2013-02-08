21c830d88f9f46c0d1c5613d6db3a56ac52f1bd1
Ilya Konovalov <i.konovalov@samsung.com>
2013-02-08 15:44:18 +0400

    Define functions to convert JTT to wrapped value and back

--- src/com/aragaer/jtt/JTTHour.java
+++ src/com/aragaer/jtt/JTTHour.java
@@ -7,6 +7,9 @@ public class JTTHour implements Parcelable {
 	public static final int QUARTERS = 4;
 	public static final int PARTS = 10; // split quarter to that much parts
 
+	public static final int HOUR_PARTS = QUARTERS * PARTS;
+	private static final int DAY_QUARTERS = 12 * QUARTERS;
+
 	public static final String Glyphs[] = { "酉", "戌", "亥", "子", "丑", "寅", "卯",
 			"辰", "巳", "午", "未", "申" };
 
@@ -58,5 +61,47 @@ public class JTTHour implements Parcelable {
 		dest.writeInt(num);
 		dest.writeInt(quarter);
 		dest.writeInt(quarter_parts);
+	}
+
+	/*
+	 * Wrapped value is a single integer denoting the JTT
+	 * Wrapping:
+	 * Wrapped value is a number of hour parts passed since last sunset
+	 * To calculate it we use SHIFTED_QUARTER
+	 * SHIFTED_QUARTER is number of quarters passed since last sunset
+	 * It is "shifted" because at sunset quarter number is QUARTERS/2
+	 * SHIFTED_QUARTER = (HOUR * QUARTERS + QUARTER + QUARTER_SHIFT) % DAY_QUARTERS
+	 * WRAPPED = SHIFTED_QUARTER * PARTS + PART
+	 *
+	 * Unwrapping:
+	 * PART = WRAPPED % PARTS
+	 * SHIFTED_QUARTER = WRAPPED / PARTS
+	 * QUARTER = (SHIFTED_QUARTER + QUARTERS / 2) % QUARTERS
+	 * HOUR = (SHIFTED_QUARTER + QUARTERS / 2) / QUARTERS
+	 */
+
+	/* I'd do simple subtraction, but it can get bad near zero since % keeps sign */
+	private static final int QUARTER_SHIFT = DAY_QUARTERS - QUARTERS / 2;
+
+	public static int wrap(int n, int q, int p) {
+		final int shifted_q = (n * QUARTERS + q + QUARTER_SHIFT) % DAY_QUARTERS;
+		return shifted_q * PARTS + p;
+	}
+
+	/* wrap self */
+	public int wrap() {
+		return wrap(num, quarter, quarter_parts);
+	}
+
+	public static JTTHour unwrap(int wrapped) {
+		return unwrap(wrapped, null);
+	}
+
+	public static JTTHour unwrap(int wrapped, JTTHour reuse) {
+		if (reuse == null)
+			reuse = new JTTHour(0);
+		final int unshifted_q = wrapped / PARTS + QUARTERS / 2; // that is shifted_q + shift back
+		reuse.setTo(unshifted_q / QUARTERS, unshifted_q % QUARTERS, wrapped % PARTS);
+		return reuse;
 	}
 }
